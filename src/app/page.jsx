cbf932305805b0d8e1aeb9f915e7872d2e9329a0
abmnet <suporteti@abmnet.org.br>
2025-10-02 19:45:10 -0300

    Módulo 2: Fundamentos

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './page.module.css'
+
+function Home() {
+  return (
+    <div className={styles.main}>Hello World!
+      <p>Teste</p>
+      <p>Teste 2</p>
+      <p>Teste 3.1</p>
+      <p>Teste Via Web</p>
+    </div>
+  )
+}
+
+export default Home
